92f05990bfac75009114841ff3436af3330dfcba
arkamaprdxn <arkama@prdxn.com>
2020-05-20 10:34:36 +0530

    hamburger function add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
 /* Author: Mohd Arkam Ansari*/
+
+// Hamburger function start here------------------------------------------
+
+var hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener('click',function(){
+  var navigationUl = document.querySelector('nav ul');
+  if(navigationUl.classList.contains('navScale')) {
+    navigationUl.classList.remove('navScale');
+    hamburger.classList.remove('hamburger-active');
+  } else {
+    navigationUl.classList.add('navScale');
+    hamburger.classList.add('hamburger-active');
+  }
+})
+
+// Hamburger function ends here---------------------------------------------
