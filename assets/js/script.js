1309a2a76a43226549ae8ca6e76ec17e112ad904
arkamaprdxn <arkama@prdxn.com>
2020-05-20 10:39:51 +0530

    accordions function add

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,44 @@ hamburger.addEventListener('click',function(){
 })
 
 // Hamburger function ends here---------------------------------------------
+
+
+// Accordion Function Starts -------------------------
+toggleAccordion();
+
+function toggleAccordion() {
+    var accordions = document.getElementsByClassName('accordion-title');
+    var accordionsData = document.getElementsByClassName('accordion-data');
+    var accordionsLength = accordions.length;
+
+    for (var i = 0; i < accordionsLength; i++) {
+        accordions[i].onclick = function () {
+            var content = this.nextElementSibling;
+            // accordion is currently open, so close it
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+                this.classList.remove('active');
+                this.firstElementChild.classList.remove('active');
+            } else { // accordion is currently closed, so open it
+                content.style.maxHeight = content.scrollHeight + 'px';
+                this.classList.add('active');
+                this.firstElementChild.classList.add('active');
+            }
+
+            // Add/Remove codes below this line for single/multiple opened accordions
+            for (var i = 0; i < accordionsLength; i++) {
+                if (accordionsData[i].style.maxHeight) {
+                    // This will remove all the properties
+                    accordionsData[i].style.maxHeight = null;
+                    accordions[i].classList.remove('active');
+                    accordions[i].firstElementChild.classList.remove('active');
+                    // This will add the properties on clicked accordion
+                    content.style.maxHeight = content.scrollHeight + 'px';
+                    this.classList.add('active');
+                    this.firstElementChild.classList.add('active');
+                }
+            } // Remove till here if you want multiple accordion
+        }
+    }
+}
+// -------------------------------------------------------
